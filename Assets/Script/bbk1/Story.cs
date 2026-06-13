a45c19e55992771a74342e4d507cd8b659388944
Ebin Diarta Pratama <ebindiarta16@gmail.com>
2026-06-13 15:20:02 +0700

    done

--- Assets/Script/bbk1/Story.cs
+++ Assets/Script/bbk1/Story.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 public class Story : MonoBehaviour
 {
     public Image bingkai;
+    public Image fadePanel;
     public Sprite[] tutorial;
 
     private int index = 0;
 
     private void Start()
     {
+        bingkai.gameObject.SetActive(true);
         StartCoroutine(BanjirCoroutine());
     }
 
@@ -18,13 +20,61 @@ public class Story : MonoBehaviour
     {
         while (index < tutorial.Length)
         {
+            yield return StartCoroutine(FadeIn());
+
             bingkai.sprite = tutorial[index];
+
+            yield return StartCoroutine(FadeOut());
 
             yield return new WaitForSeconds(2f);
 
             index++;
         }
-
+        yield return StartCoroutine(FadeIn());
         bingkai.gameObject.SetActive(false);
+        yield return StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeIn()
+{
+    float t = 0;
+
+    Vector3 posisiAwal = bingkai.rectTransform.localPosition;
+
+    while (t < 1)
+    {
+        t += Time.deltaTime * 2f;
+
+        Color c = fadePanel.color;
+        c.a = Mathf.Lerp(0, 1, t);
+        fadePanel.color = c;
+        if (t > 0.5f)
+        {
+            bingkai.rectTransform.localPosition =
+                posisiAwal + new Vector3(
+                    Random.Range(-10f, 10f),
+                    Random.Range(-10f, 10f),
+                    0);
+        }
+
+        yield return null;
+    }
+
+    bingkai.rectTransform.localPosition = posisiAwal;
+}
+    IEnumerator FadeOut()
+    {
+        float t = 0;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime * 2f;
+
+            Color c = fadePanel.color;
+            c.a = Mathf.Lerp(1, 0, t);
+            fadePanel.color = c;
+
+            yield return null;
+        }
     }
 }
--- Assets/Script/cut scene/jmbtntopsr.cs
+++ Assets/Script/cut scene/jmbtntopsr.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class jmbtntopsr : MonoBehaviour
 {
     public GameObject cutscene;
+    public GameObject cutscene2;
+    public GameObject zone;
     public Transform mc;
     public Transform tujuan;
 
     void Start()
     {
         cutscene.SetActive(false);
+        cutscene2.SetActive(false);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             mc.transform.position = tujuan.transform.position;
-            cutscene.SetActive(true);
+            zone.SetActive(false);
+            if (GameClock.instance.currentDay < 3)
+            {
+                cutscene.SetActive(true);
+                cutscene2.SetActive(false);
+            } else if(GameClock.instance.currentDay >= 3)
+            {
+                cutscene.SetActive(false);
+                cutscene2.SetActive(true);
+            }
         }
     }
 }
