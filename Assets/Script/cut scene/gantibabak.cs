e16b980eb975853c598aa416eb53c0499f03d5fa
Ebin Diarta Pratama <ebindiarta16@gmail.com>
2026-06-16 10:53:33 +0700

    done

--- Assets/Script/cut scene/gantibabak.cs
+++ Assets/Script/cut scene/gantibabak.cs
@@ -5,11 +5,15 @@ using UnityEngine.SceneManagement;
 public class GantiBabak : MonoBehaviour
 {
     public GameObject cutscene;
+    Spawner spawner;
     public Transform mc;
     public Transform tujuan;
 
     
-
+    void Start()
+{
+    spawner = FindObjectOfType<Spawner>();
+}
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -23,6 +27,10 @@ public class GantiBabak : MonoBehaviour
         cutscene.SetActive(true);
         yield return new WaitForSeconds(3f);
         GameClock.instance.gantibbk();
+        if(spawner != null)
+        {
+            spawner.RespawnTrash();
+        }
         mc.position = tujuan.position;
         cutscene.SetActive(false);
         gameObject.SetActive(false);
